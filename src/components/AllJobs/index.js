7fb0bd21ba304aa68ff7ce9f81c4b53a5cfaa0af
Harivaradhan KM <harivaradhank@gmail.com>
2023-09-07 13:14:12 +0000

    midupdate_As5

---
+++ src/components/AllJobs/index.js
@@ -0,0 +1,174 @@
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import {AiOutlineSearch} from 'react-icons/ai'
+import Cookies from 'js-cookie'
+import JobCardItem from '../JobCardItem'
+import Header from '../Header'
+import './index.css'
+
+const employmentTypesList = [
+  {
+    label: 'Full Time',
+    employmentTypeId: 'FULLTIME',
+  },
+  {
+    label: 'Part Time',
+    employmentTypeId: 'PARTTIME',
+  },
+  {
+    label: 'Freelance',
+    employmentTypeId: 'FREELANCE',
+  },
+  {
+    label: 'Internship',
+    employmentTypeId: 'INTERNSHIP',
+  },
+]
+
+const salaryRangesList = [
+  {
+    salaryRangeId: '1000000',
+    label: '10 LPA and above',
+  },
+  {
+    salaryRangeId: '2000000',
+    label: '20 LPA and above',
+  },
+  {
+    salaryRangeId: '3000000',
+    label: '30 LPA and above',
+  },
+  {
+    salaryRangeId: '4000000',
+    label: '40 LPA and above',
+  },
+]
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAIL',
+  inProgress: 'IN_PROGRESS',
+}
+
+class AllJobs extends Component {
+  state = {
+    apiStatus: apiStatusConstants.initial,
+    profileData: {},
+    jobsData: [],
+    apiJobStatus: apiStatusConstants.initial,
+    activeCheckBoxList: [],
+    activeSalaryRange: '',
+    searchInput: '',
+  }
+
+  componentDidMount() {
+    this.getProfileData()
+    this.getJobsData()
+  }
+
+  getProfileData = async () => {
+    this.setState({apiJobStatus: apiStatusConstants.inProgress})
+    const jwtToken = Cookies.get('jwt_token')
+    const url = 'https://api.ccbp.in/profile'
+    const option = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    }
+    const response = await fetch(url, option)
+    const data = await response.json()
+    console.log(data)
+    if (response.ok === true) {
+      const profile = data.profile_details
+      const updatedProfileData = {
+        name: profile.name,
+        profileImageUrl: profile.profie_image_url,
+        shortBio: profile.short_bio,
+      }
+      console.log(updatedProfileData)
+      this.setState({
+        profileData: updatedProfileData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  getJobsData = async () => {
+    this.setState({apiJobStatus: apiStatusConstants.inProgress})
+    const {activeCheckBoxList, activeSalaryRange, searchInput} = this.state
+    const type = activeCheckBoxList.join(',')
+    const jwtToken = Cookies.get('jwt_token')
+    const url = `https://api.ccbp.in/jobs?employment_type=${type}&minimum_package=${activeSalaryRangeId}&search=${search}`
+    const option = {
+      header: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, option)
+    const data = await response.json()
+    console.log(data)
+    if (response.ok === true) {
+      const filteredJobsList = data.jobs.map(each => ({
+        id: each.id,
+        companyLogoUrl: each.company_logo_url,
+        employmentType: each.employment_type,
+        jobDescription: each.job_description,
+        location: each.location,
+        packagePerAnnum: each.package_per_annum,
+        rating: each.rating,
+        title: each.title,
+      }))
+      console.log(filteredJobsList)
+
+      this.setState({
+        jobsData: filteredJobsList,
+        apiJobStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiJobStatus: apiStatusConstants.failure})
+    }
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onsubmitSearchInput = () => {
+    this.getJobsData()
+  }
+
+  onEnterSearchInput = event => {
+    if (event.key === 'Enter') {
+      this.getJobsData()
+    }
+  }
+
+  onSelectSalaryRage = event => {
+    this.setState({activeSalaryRange: event.target.id}, this.getJobsData)
+  }
+
+  onClickCheckbox = event => {
+    const {activeCheckBoxList} = this.state
+    if (activeCheckBoxList.includes(event.target.id)) {
+      const updatedList = activeCheckBoxList.filter(
+        each => each !== event.target.id,
+      )
+      this.setState({activeCheckBoxList: updatedList}, this.getJobsData)
+    } else {
+      this.setState(
+        prevState => ({
+          activeCheckBoxList: [
+            ...prevState.activeCheckBoxList,
+            event.target.id,
+          ],
+        }),
+        this.getJobsData,
+      )
+    }
+  }
+}
